d21693589684438da89d876e3dec1aea19c5cb57
DeepakVetrivel97 <deepak.v6@cognizant.com>
2026-06-25 17:59:01 +0530

    Update c1_versions.tf

--- 02_EKS_terraform-manifests/c1_versions.tf
+++ 02_EKS_terraform-manifests/c1_versions.tf
@@ -21,6 +21,11 @@ terraform {
       source  = "hashicorp/http"
       version = "~> 3.5.0"
     }    
+    
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
   }
 
   # Remote backend configuration using S3 
